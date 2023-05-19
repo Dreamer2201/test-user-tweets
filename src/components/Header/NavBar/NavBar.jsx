484a54c0228bc56db1cbcff40751e97906d20337
Oksana Polyakova <ddreamer22011986@gmail.com>
2023-05-19 11:33:42 +0300

    ADDED: logo and change favicon

--- src/components/Header/NavBar/NavBar.jsx
+++ src/components/Header/NavBar/NavBar.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import {AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import HowToRegIcon from '@mui/icons-material/HowToReg';
 import {NavBarLink} from './NavBar.styled'
-
+import imgLogo from '../../../images/Website-Icon.png'
 
 export default function NavBar() {
   return (
     <AppBar position="static">
       <Toolbar>
           <IconButton edge="start">
-            <HowToRegIcon />
+            <Link to='/'>
+            <img src={imgLogo} />  
+            </Link>
+                    
           </IconButton>
           <NavBarLink to='/'>Home</NavBarLink>
         <NavBarLink to='/tweets'>Tweets</NavBarLink>
